842cb2466e55ae179b03855b230b90842282a3e8
gagetstowers <38923134+gagetstowers@users.noreply.github.com>
2020-04-22 02:11:33 -0400

    updated leaderboard

---
+++ SSAD Game/Assets/Integration/Leaderboard/LeaderBoardEntry.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+
+public class LeaderBoardEntry {
+    public string uid;
+    public int score = 0;
+
+    public LeaderBoardEntry() {
+    }
+
+    public LeaderBoardEntry(string uid, int score) {
+        this.uid = uid;
+        this.score = score;
+    }
+
+    public Dictionary<string, System.Object> ToDictionary() {
+        Dictionary<string, System.Object> result = new Dictionary<string, System.Object>();
+        result["uid"] = uid;
+        result["score"] = score;
+
+        return result;
+    }
+}
--- SSAD Game/Assets/Integration/Leaderboard/Leaderboard.cs
+++ SSAD Game/Assets/Integration/Leaderboard/Leaderboard.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
@@ -7,7 +8,14 @@ using Firebase.Unity.Editor;
 
 public class Leaderboard : MonoBehaviour
 {
-    DatabaseReference reference;
+
+
+    //score of last game played
+
+    public int recentScore = 00000;
+    //type of last game played ('puzzle' or 'hydro')
+    public string recentType = "error";
+    public string playerName = "Player";
 
     [SerializeField]
     private TextMesh scoreOne;
@@ -20,51 +28,88 @@ public class Leaderboard : MonoBehaviour
     [SerializeField]
     private TextMesh scoreFive;
 
-    private string name = "Gage";
-    private string score = "69";
+    public int[] scores = { 0, 0, 0, 0, 0 };
+    public string[] usernames = { "x", "x", "x", "x", "x" };
+    private int maxScores = 5;
+
+    DatabaseReference reference;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        
+        //email me at gstowers@elon.edu if u wanna see the database
         // Set up the Editor before calling into the realtime database.
-        //email me at gstowers@elon.edu for database permissions
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://integration-db.firebaseio.com/");
 
         // Get the root reference location of the database.
-        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
-        reference.Child("Name").SetValueAsync(name);
-        reference.Child("Value").SetValueAsync(score);
-        scoreOne.text="xxx";
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        WriteNewScore("Gage", 69);
+        WriteNewScore("GOd", 80);
+        WriteNewScore("GOd", 85);
+        WriteNewScore("GOd", 90);
+        WriteNewScore("GOd", 74);
+        WriteNewScore("GOd", 30);
+        WriteNewScore("GOd", 28);
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       // LoadData();
-    }
-    public void SaveData()
-    {
+        //retrieves highest five scores
+        FirebaseDatabase.DefaultInstance
+       .GetReference("scores").OrderByChild("score").LimitToLast(maxScores)
+       .ValueChanged += HandleValueChanged;
 
+        //updates scoreboard values
+        scoreOne.text = usernames[4] + ": " + scores[4].ToString();
+        scoreTwo.text = usernames[3] + ": " + scores[3].ToString();
+        scoreThree.text = usernames[2] + ": " + scores[2].ToString();
+        scoreFour.text = usernames[1] + ": " + scores[1].ToString();
+        scoreFive.text = usernames[0] + ": " + scores[0].ToString();
+    }
+
+    //checks for updates automatically
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+        int i = 0;
+        foreach (DataSnapshot leader in args.Snapshot.Children)
+        {
+            scores[i] = Convert.ToInt32(leader.Child("score").Value);
+            usernames[i] = leader.Child("uid").Value.ToString();
+            i++;
+        }
+
+
+    }
+
+
+    //enter username and score 
+    private void WriteNewScore(string userId, int score)
+    {
       
-    }
-    public void LoadData()
-    {
-        FirebaseDatabase.DefaultInstance.GetReference("Name").ValueChanged += HandleValueChanged;
+        string key = score.ToString();
+        LeaderBoardEntry entry = new LeaderBoardEntry(userId, score);
+        Dictionary<string, System.Object> entryValues = entry.ToDictionary();
 
-    }
-    public void HandleScores(){
-       // FirebaseDatabase.DefaultInstance.orderByChild("Value").LimitToFirst(5).ValueChanged+=HandleValueChanged;
-    }
+        Dictionary<string, System.Object> childUpdates = new Dictionary<string, System.Object>();
+        childUpdates["/scores/" + key] = entryValues;
 
 
+        reference.UpdateChildrenAsync(childUpdates);
 
-    private void HandleValueChanged(object sender, ValueChangedEventArgs args)
-    {
-       if(args.DatabaseError!=null){
-           Debug.LogError(args.DatabaseError.Message);
-           return;
-       }
-      // scoreOne.text=args.Snapshot("Name").GetValue(true).ToString();
     }
+
+
 }
 
